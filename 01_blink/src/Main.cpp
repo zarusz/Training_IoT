975874aa205907f5bcf21338512e5c87fd36d65d
Tomasz Maruszak <maruszaktomasz@gmail.com>
2016-08-13 17:01:11 +0200

    The blink example.

---
+++ 01_blink/src/Main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+#include <Arduino.h>
+
+#define LED 12
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED, LOW);
+   // wait for a second
+  delay(1000);
+}
